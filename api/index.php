c784ab4977926d5f9b46c3e0906c75ece53e1ccf
hafiz4902 <septianuh@gmail.com>
2026-06-07 14:02:16 +0700

    fix: set vercel storage path to /tmp

--- api/index.php
+++ api/index.php
@@ -5,4 +5,14 @@
  * Meneruskan request dari Vercel serverless function ke file public/index.php Laravel
  */
 
+$storage = '/tmp/storage';
+if (!is_dir($storage)) {
+    mkdir($storage, 0777, true);
+    mkdir($storage.'/framework/views', 0777, true);
+    mkdir($storage.'/framework/cache', 0777, true);
+    mkdir($storage.'/framework/sessions', 0777, true);
+    mkdir($storage.'/logs', 0777, true);
+}
+$_ENV['APP_STORAGE'] = $storage;
+
 require __DIR__ . '/../public/index.php';
