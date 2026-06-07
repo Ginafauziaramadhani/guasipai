80a8581f6b5bb5fc4e792bc5c2f6576702877ef5
hafiz4902 <septianuh@gmail.com>
2026-06-07 13:30:25 +0700

    prepare for deploy on vercel

---
+++ api/index.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Vercel Serverless Entry Point
+ * Meneruskan request dari Vercel serverless function ke file public/index.php Laravel
+ */
+
+require __DIR__ . '/../public/index.php';
